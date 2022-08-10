9a2355bca35aea0f799db04254f2bd1500e6e245
Rag404 <105586263+Rag404@users.noreply.github.com>
2022-08-10 23:57:36 +0200

    fixed paths in config file
    Paths weren't working if "tavernier_main.py" wasn't run directly from the "tavernier_bot" directory
    (You basically had to:
      cd full/path/to/tavernier_bot
    before running the bot)

--- data/config.py
+++ data/config.py
@@ -1,5 +1,9 @@
 import discord
 
+# Dirty way to get the root directory (removes '\data\config.py')
+root_path = __file__[:-15]
+
+
 # Global
 TAVERN_ID = 807743905121566720
 OWNER_ID = 576435921390403623
@@ -19,19 +23,19 @@ BOT_EXTENSIONS = [
 ]
 
 # Error handling
-TRACEBACK_FILE_PATH = './data/last_traceback.txt'
+TRACEBACK_FILE_PATH = root_path + '/data/last_traceback.txt'
 
 # Infochannels
 MEMBERS_INFOCHANNEL = 925362480430059541
 ONLINES_INFOCHANNEL = 963536054621716520
 
 # Reaction-roles
-REACTION_ROLES_PATH = './data/reaction_roles_test.json'
+REACTION_ROLES_PATH = root_path + '/data/reaction_roles.json'
 
 # Voice rooms
 REDIRECT_VOICE_CHANNEL = 996160558371979355
 ROOMS_CATEGORY = 996159603324768276
-ROOMS_SAVE_PATH = './data/rooms_save.json'
+ROOMS_SAVE_PATH = root_path + '/data/rooms_save.json'
 ROOM_LEADER_OVERWRITES = discord.PermissionOverwrite(manage_channels=True, manage_permissions=True, move_members=True, mute_members=True, deafen_members=True, manage_events=True)
 
 # Welcome

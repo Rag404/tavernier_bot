78420f0a34155941735ef28efdf0d3aeafb8d4a4
Rag404 <105586263+Rag404@users.noreply.github.com>
2022-12-26 14:45:11 +0100

    fixed errors and bugs

--- cogs/hyperactive.py
+++ cogs/hyperactive.py
@@ -1,41 +1,47 @@
 from discord import Cog, Bot, Member, VoiceState
-from data.config import STREAK_DATA, STREAK_WEEK_DAY, STREAK_HYPERACTIVE, HYPERACTIVE_ROLE, STREAK_TIME_MIN, REDIRECT_VOICE_CHANNEL
+from data.config import STREAK_DATA, STREAK_WEEK_DAY, STREAK_HYPERACTIVE, HYPERACTIVE_ROLE, STREAK_TIME_MIN, REDIRECT_VOICE_CHANNEL, STREAK_DB_COLLECTION
 from resources.database import database
 from resources.utils import log
 from typing import Union
 import datetime as dt
 
+col = database.get_collection(STREAK_DB_COLLECTION)
 
-col = database.get_collection("hyperactive")
 
-
-def read_data(member: Member = None, *, field: str = None) -> Union[dict, int, float]:
+def read_data(member: Member = None, *, field: str = None) -> Union[dict, int, float, list]:
     """Get either the entire data, a member's data, or only a field"""
     
     if member:
         value = col.find_one({"_id": member.id})
         
         if value is None: # Handling member not already known
-            value = col.insert_one({"_id": member.id} | STREAK_DATA)
+            value = {"_id": member.id} | STREAK_DATA
+            col.insert_one(value)
         
         if field:
-            value = value.get(field)
+            value = value[field]
         
         return value
     
     else:
-        return col.find({})
+        return list(col.find({}))
 
 
 def update_data(member: Member, member_data, *, field: str = None):
     """Update either the data, or only a field, of a member"""
     
     if field:
-        assert type(member_data) in (int, float)
-        col.update_one({"_id": member.id}, {"$set": {field: member_data}})
+        assert type(member_data) in (int, float) and member_data >= 0
+        result = col.update_one({"_id": member.id}, {"$set": {field: member_data}})
     else:
         assert type(member_data) is dict and member_data.keys() == STREAK_DATA.keys()
-        col.update_one({"_id": member.id}, {"$set": member_data})
+        result = col.update_one({"_id": member.id}, {"$set": member_data})
+    
+    if result.modified_count == 0:
+        if field:
+            col.insert_one(STREAK_DATA | {"_id": member.id, field: member_data})
+        else:
+            col.insert_one(STREAK_DATA | {"_id": member.id} | member_data)
 
 
 async def reset_progress(member: Member):
@@ -79,7 +85,7 @@ async def update_time(member: Member):
     last = dt.datetime.fromtimestamp(member_data.get("last"))
     now = dt.datetime.utcnow()
     
-    session = (now - last) / dt.timedelta(hours=1)  # Convert into hours
+    session = to_hours(now - last)
     time = member_data.get("time", 0) + session
     update_data(member, time, field="time")
     
@@ -95,7 +101,7 @@ def add_time(member: Member, value: Union[float, dt.timedelta]):
     if type(value) is float:
         time += value
     elif type(value) is dt.timedelta:
-        time += value / dt.timedelta(hours=1)
+        time += to_hours(value)
     
     update_data(member, time, field="time")
 
@@ -115,8 +121,10 @@ async def increase_streak(member: Member):
 def streak_day() -> dt.datetime:
     """Return a datetime object for the last day where streaks have been updated"""
 
-    now = dt.datetime.utcnow()
-    return now + dt.timedelta(days=STREAK_WEEK_DAY - now.weekday())
+    now = dt.datetime.utcnow().date()
+    days = abs(STREAK_WEEK_DAY - now.weekday())
+    day = now - dt.timedelta(days=days)
+    return dt.datetime.combine(day, dt.time())
 
 
 async def handle_midnight(member):
@@ -131,8 +139,13 @@ async def handle_midnight(member):
     else:
         await reset_progress(member)
     
-    update_data(member, now - streak_day(), "time")
+    time = to_hours(now - streak_day())
+    update_data(member, time, field="time")
 
+
+def to_hours(delta: dt.timedelta) -> float:
+    """Convert a timedelta object into a number of hours"""
+    return delta / dt.timedelta(hours=1)
 
 
 class Hyperactive(Cog):

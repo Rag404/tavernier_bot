75a5e9360584892cadcc86e53bf55ef7309da619
Rag404 <105586263+Rag404@users.noreply.github.com>
2023-04-26 13:47:32 +0200

    Fixed hyperactive time display bug
    FIxed a bug where times higher than 24h would get truncated
    Time now displays in hours instead of days

--- resources/utils.py
+++ resources/utils.py
@@ -10,8 +10,7 @@ def log(*values: object, **kwargs):
 
 def time2str(time: dt.timedelta):
     values = {}
-    values["j"], r = divmod(time.seconds, 86400)
-    values["h"], r = divmod(r, 3600)
+    values["h"], r = divmod(round(time.total_seconds()), 3600)
     values["min"], values["s"] = divmod(r, 60)
     
     return " ".join([str(v) + k for k, v in values.items() if v > 0]) or "0s"
